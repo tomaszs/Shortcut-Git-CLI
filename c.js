19deaee5783abba89ef0bf1810442e5eba23c620
Tomasz Smykowski <tomasz_smykowski@o2.pl>
2022-05-13 14:15:46 +0200

    Update c.js

--- c.js
+++ c.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+const shortcutToken = 'YOUR-SHORTCUT_TOKEN';
+const ticketQuery = 'owner:YOUR_HANDLE state:"In Progress"';
+
+function saveStoryHook() {
+  // A place for fancy stuff when story is saved
+}
+
 const fetch = require('node-fetch');
-const shortcutToken = 'YOUR_SHORTCUT_TOKEN';
 const execSync = require('child_process').execSync;
 const query = require('cli-interact').getInteger;
 
@@ -16,6 +22,7 @@ function titleCase(str) {
 function formatStoryName(storyName, storyId, storyType) {
   let storyNameTrimmed = storyName;
 
+  storyNameTrimmed = storyNameTrimmed.replaceAll(/[^A-Za-z0-9-!?]/g, '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('update', '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('use', '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('always', '');
@@ -23,14 +30,6 @@ function formatStoryName(storyName, storyId, storyType) {
   storyNameTrimmed = storyNameTrimmed.replaceAll('do ', ' ');
   storyNameTrimmed = storyNameTrimmed.replaceAll('not ', ' ');
   storyNameTrimmed = storyNameTrimmed.replaceAll('error', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll(':', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll('`', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll('\'', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll('\'', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll('[', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll(']', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll('"', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll('\'', '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('investigate', '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('referenceerror', '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('the', '');
@@ -45,12 +44,11 @@ function formatStoryName(storyName, storyId, storyType) {
   storyNameTrimmed = storyNameTrimmed.replaceAll('of ', ' ');
   storyNameTrimmed = storyNameTrimmed.replaceAll('replace ', '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('Replace ', '');
-  storyNameTrimmed = storyNameTrimmed.replaceAll('*', '');
   storyNameTrimmed = storyNameTrimmed.replaceAll('to ', ' ');
+
   storyNameTrimmed = storyNameTrimmed.replaceAll('----', '-');
   storyNameTrimmed = storyNameTrimmed.replaceAll('---', '-');
   storyNameTrimmed = storyNameTrimmed.replaceAll('--', '-');
-
   var shortStoryName = storyNameTrimmed.substring(0, 10);
   shortStoryName = shortStoryName.replaceAll('--', '-');
   shortStoryName = shortStoryName.replaceAll('"', '');
@@ -102,6 +100,16 @@ function silentCall(command) {
   return success;
 }
 
+function silentCallResponse(command) {
+  const response = call(command);
+  const success = !response.toString().includes('Error');
+  if (!success) {
+    throw response;
+  }
+
+  return response;
+}
+
 async function getStories(query) {
   const url = 'https://api.app.shortcut.com/api/v3/search/stories';
 
@@ -111,6 +119,7 @@ async function getStories(query) {
     }
   };
   const params = new URLSearchParams(query);
+
   return fetch(`${url}?${params}`, settings)
     .then(res => res.json()).then(res => res.data);
 }
@@ -121,9 +130,9 @@ function doesBranchExist(branch) {
   );
 }
 
-async function process() {
+async function switchStory() {
   const stories = await getStories({ page_size: 1,
-    query: 'owner:tomaszsmykowski state:"In Progress"' });
+    query: ticketQuery });
 
   console.log('');
   stories.forEach((story, index) => {
@@ -155,4 +164,63 @@ async function process() {
   }
 }
 
-process();
+async function findTicketName(branchName) {
+  const stories = await getStories({ page_size: 1,
+    query: ticketQuery });
+  for (let i = 0; i < stories.length; i++) {
+    const selectedStory = stories[i];
+    const formattedNames = formatStoryName(selectedStory.name,
+      selectedStory.id, selectedStory.story_type);
+    if (formattedNames.branch === branchName) {
+      return formattedNames.ticket;
+    }
+  }
+
+  return 'done';
+}
+
+async function saveStory() {
+  try {
+    silentCall('git add -A');
+    const branchName = silentCallResponse('git rev-parse --abbrev-ref HEAD');
+    const ticketName = await findTicketName(branchName);
+    silentCall(`git commit -a -m "${ticketName}"`);
+    silentCall('git push');
+    if (saveStoryHook) {
+      saveStoryHook();
+    }
+
+    console.log(`Changes saved to ${ticketName}`);
+  } catch (e) {
+    if (e.toString().includes('No staged files found')) {
+      console.log('No changes to Save');
+    } else {
+      console.log(e);
+    }
+  }
+}
+
+async function chooseOption() {
+  const options = {
+    0: 'Exit',
+    1: 'Switch',
+    2: 'Save'
+  };
+  Object.keys(options).forEach(key => console.log(`${key}. ${options[key]}`));
+  const response = query('What you want to do? ');
+  if (Object.keys(options).includes(response.toString())) {
+    if (response === 1) {
+      await switchStory();
+    } else if (response === 2) {
+      await saveStory();
+    } else if (response === 0) {
+      process.exit(0);
+    }
+
+    if (response > 0) {
+      chooseOption();
+    }
+  }
+}
+
+chooseOption();
